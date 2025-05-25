262548c7d271c4bc654df2b40481200933cbbd56
AlcantuMurilo77 <157785297+AlcantuMurilo77@users.noreply.github.com>
2025-05-25 03:17:46 -0400

    Add try/except to handle database connection errors

--- utils/database.py
+++ utils/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.exc import SQLAlchemyError
 from utils.config import Config
 from dotenv import load_dotenv
 
 load_dotenv(".env", override=True)
 
-engine = create_engine(Config.url, echo=False)
+try:
+    engine = create_engine(Config.url, echo=False)
 
-SessionLocal = scoped_session(sessionmaker(bind=engine, autoflush=False, autocommit=False))
+    with engine.connect() as connection:
+        connection.execute("SELECT 1")
+    
+    SessionLocal = scoped_session(sessionmaker(bind=engine, autoflush=False, autocommit=False))
 
+except SQLAlchemyError as e:
+    raise RuntimeError(f"[DB ERROR] Error conecting to PostgresDB: {e}")
+except Exception as e:
+    raise RuntimeError(f"[UNEXPECTED ERROR] Unexpected error on initializing engie: {e}")
