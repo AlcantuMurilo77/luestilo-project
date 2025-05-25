5032a155500e06004b29c7dd52eca06dbeeecc66
AlcantuMurilo77 <157785297+AlcantuMurilo77@users.noreply.github.com>
2025-05-25 00:29:54 -0400

    Add order schema test

---
+++ tests/schemas/order_schema_test.py
@@ -0,0 +1,11 @@
+import pytest
+from pydantic import ValidationError
+from utils.schemas.order import OrderCreate
+
+def test_valid_order_create():
+    order = OrderCreate(client_id=1)
+    assert order.client_id == 1
+
+def test_missing_client_id():
+    with pytest.raises(ValidationError):
+        OrderCreate()
