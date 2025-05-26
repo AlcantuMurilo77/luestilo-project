29113cea42c0485109d1abd8777d3a9fb5a5ad93
Murilo Alcantu <157785297+AlcantuMurilo77@users.noreply.github.com>
2025-05-26 03:30:16 -0300

    Create user model

--- models/models.py
+++ models/models.py
@@ -81,3 +81,10 @@ class OrderProduct(Base):
 
     order = relationship("Order", back_populates="products")
     product = relationship("Product")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
